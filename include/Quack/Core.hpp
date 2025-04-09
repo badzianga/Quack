024a9ba66df3140815aa020d1a9f335f6073a1bf
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-04-09 12:52:28 +0200

    Headers with whole module includes

---
+++ include/Quack/Core.hpp
@@ -0,0 +1,10 @@
+#ifndef QUACK_CORE_HPP
+#define QUACK_CORE_HPP
+
+#include "Quack/Core/Engine.hpp"
+#include "Quack/Core/Input.hpp"
+#include "Quack/Core/Keyboard.hpp"
+#include "Quack/Core/Time.hpp"
+#include "Quack/Core/Window.hpp"
+
+#endif //QUACK_CORE_HPP
---
+++ include/Quack/Graphics.hpp
@@ -0,0 +1,8 @@
+#ifndef QUACK_GRAPHICS_HPP
+#define QUACK_GRAPHICS_HPP
+
+#include "Quack/Graphics/Mesh.hpp"
+#include "Quack/Graphics/Shader.hpp"
+#include "Quack/Graphics/Vertex.hpp"
+
+#endif //QUACK_GRAPHICS_HPP
