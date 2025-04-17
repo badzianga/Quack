0a52e0b83ec6b56247d25272c79eb1b34754dd0f
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-04-17 18:50:56 +0200

    Delete unused getAllComponents method

--- include/Quack/Scene/Scene.hpp
+++ include/Quack/Scene/Scene.hpp
@@ -13,21 +13,6 @@ public:
     void startAllGameObjects() const;
 
     void updateAllGameObjects() const;
-
-    // TODO: Currently unused
-    // template <typename T>
-    // std::vector<T*> getAllComponents() {
-    //     std::vector<T*> result;
-    //     for (auto& gameObject : m_gameObjects) {
-    //         if (gameObject->isActive()) {
-    //             T* component = gameObject->getComponent<T>();
-    //             if (component != nullptr) {
-    //                 result.push_back(component);
-    //             }
-    //         }
-    //     }
-    //     return result;
-    // }
 private:
     std::vector<std::unique_ptr<GameObject>> m_gameObjects;
 };
