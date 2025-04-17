7bd264defe4a0b291c53f97aca91f4421fb829d8
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-04-17 18:50:01 +0200

    Utils module documentation

--- include/Quack/Utils/FileIO.hpp
+++ include/Quack/Utils/FileIO.hpp
@@ -2,15 +2,44 @@
 #define QUACK_FILE_IO_HPP
 #include <string>
 
+/**
+ * @brief Static utility class for file input.
+ *
+ * Provides a simple interface for reading files as text.
+ */
 class FileIO {
 public:
+    /**
+     * @brief Deleted default constructor.
+     */
     FileIO() = delete;
-    ~FileIO() = delete;
+
+    /**
+     * @brief Deleted copy constructor.
+     */
     FileIO(const FileIO&) = delete;
+
+    /**
+     * @brief Deleted move constructor.
+     */
     FileIO(FileIO&&) = delete;
+
+    /**
+     * @brief Deleted copy assignment.
+     */
     FileIO& operator=(const FileIO&) = delete;
+
+    /**
+     * @brief Deleted move assignment.
+     */
     FileIO& operator=(FileIO&&) = delete;
 
+    /**
+     * @brief Read the entire contents of a text file.
+     *
+     * @param filename Path to the file.
+     * @return File content as a string.
+     */
     static std::string read(const char* filename);
 };
 
--- include/Quack/Utils/Logger.hpp
+++ include/Quack/Utils/Logger.hpp
@@ -3,24 +3,89 @@
 #include <mutex>
 #include <string>
 
+/**
+ * @brief Static logger utility for engine-wide logging.
+ *
+ * Provides thread-safe logging with different log levels.
+ * Supports filtering by minimum log level.
+ */
 class Logger {
 public:
+    /**
+     * @enum LogLevel
+     * @brief Represents the severity of a log message.
+     */
     enum class LogLevel { Debug, Info, Warning, Error };
 
+    /**
+     * @brief Deleted default constructor.
+     */
     Logger() = delete;
-    ~Logger() = delete;
+
+    /**
+     * @brief Deleted copy constructor.
+     */
     Logger(const Logger&) = delete;
+
+    /**
+     * @brief Deleted move constructor.
+     */
     Logger(Logger&&) = delete;
+
+    /**
+     * @brief Deleted copy assignment.
+     */
     Logger& operator=(const Logger&) = delete;
+
+    /**
+     * @brief Deleted move assignment.
+     */
     Logger& operator=(Logger&&) = delete;
 
+    /**
+     * @brief Log a message with a given log level.
+     *
+     * @param level Severity of the log message.
+     * @param message The message content.
+     */
     static void log(LogLevel level, const std::string& message);
 
+    /**
+     * @brief Log a debug message.
+     *
+     * @param message Message content.
+     */
     static void debug(const std::string& message);
+
+    /**
+     * @brief Log an informational message
+     *
+     * @param message Message content.
+     */
     static void info(const std::string& message);
+
+    /**
+     * @brief Log a warning message
+     *
+     * @param message Message content.
+     */
     static void warn(const std::string& message);
+
+    /**
+     * @brief Log an error message.
+     *
+     * @param message Message content.
+     */
     static void error(const std::string& message);
 
+    /**
+     * @brief Set the minimum log level.
+     *
+     * Messages below this level will be ignored.
+     * By default, log level is set to Info.
+     *
+     * @param level Minimum log level to display.
+     */
     static void setMinLogLevel(LogLevel level);
 
 private:
