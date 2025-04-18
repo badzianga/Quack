54115833a2b8de1605268c65c97dcf9d2399b76e
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-04-18 22:27:31 +0200

    Add color headers to Graphics.hpp

--- include/Quack/Graphics.hpp
+++ include/Quack/Graphics.hpp
@@ -1,6 +1,8 @@
 #ifndef QUACK_GRAPHICS_HPP
 #define QUACK_GRAPHICS_HPP
 
+#include "Quack/Graphics/Color.hpp"
+#include "Quack/Graphics/Color8.hpp"
 #include "Quack/Graphics/Material.hpp"
 #include "Quack/Graphics/Mesh.hpp"
 #include "Quack/Graphics/Shader.hpp"
